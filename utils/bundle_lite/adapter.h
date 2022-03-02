0635072b275cc9f4786a89cb855c1b1ab1563282
吴珞风 <wuluofeng@huawei.com>
2022-03-02 10:41:21 +0000

    Description:Rolling Back the Dotting Function. Sig:appexecfwk Feature or Bugfix:Bugfix Binary Source:No #I4W0YR; Signed-off-by: wuluofeng <wuluofeng@huawei.com>

--- utils/bundle_lite/adapter.h
+++ utils/bundle_lite/adapter.h
@@ -75,7 +75,8 @@ const unsigned int RETRY_TIMES = 10;
             a = nullptr; \
         } \
     } while (0)
-
+#define APP_ERRCODE_EXTRA(code1, code2)
+#define APP_EVENT(code1)
 #define RecordAbiityInfoEvt(code1)
 #define MutexDelete(a) osMutexDelete(a)
 #define MutexAcquire(a, b) osMutexAcquire(a, b)
