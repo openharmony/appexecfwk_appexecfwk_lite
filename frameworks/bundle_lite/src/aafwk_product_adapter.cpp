662166648644b63b1b56143fc1495718de0502ec
qiudengcheng <qiudengcheng@huawei.com>
2022-05-11 08:09:52 +0000

    Description:Dotting optimization Sig:appexecfwk Feature or Bugfix:Bugfix Binary Source:No #I56WWW; Signed-off-by:<qiudengcheng@huawei.com>

--- frameworks/bundle_lite/src/aafwk_product_adapter.cpp
+++ frameworks/bundle_lite/src/aafwk_product_adapter.cpp
@@ -14,67 +14,18 @@
  */
 
 #include "aafwk_product_adapter.h"
+#include "event.h"
+#include "errorcode.h"
 
 namespace OHOS {
-/**
- * Used for holding all the related dfx interfaces assigned from specific implementation.
- */
-struct AafwkDFXWrapper {
-    AafwkDFXWrapper()
-        : eventTag_(0),
-          eventSubTag_(0),
-          errCodeTag_(0),
-          errCodeSubTag_(0),
-          eventPrintHandler_(nullptr),
-          errCodePrintHandler_(nullptr)
-    {
-    }
-    uint8_t eventTag_;
-    uint8_t eventSubTag_;
-    uint8_t errCodeTag_;
-    uint8_t errCodeSubTag_;
-    EventPrintHandler eventPrintHandler_;
-    ErrCodePrintHandler errCodePrintHandler_;
-};
 
-static AafwkDFXWrapper sDfxWrapper;
-
-void AafwkProductAdapter::InitAafwkTags(uint8_t *aafwkTags, uint8_t tagCount)
+void AafwkProductAdapter::PrintEventTrace(uint8_t info1, uint8_t info2, uint8_t info3)
 {
-    const uint8_t minCount = 4;
-    if (aafwkTags == nullptr || tagCount < minCount) {
-        return;
-    }
-    uint8_t index = 0;
-    sDfxWrapper.eventTag_ = aafwkTags[index++];
-    sDfxWrapper.eventSubTag_ = aafwkTags[index++];
-    sDfxWrapper.errCodeTag_ = aafwkTags[index++];
-    sDfxWrapper.errCodeSubTag_ = aafwkTags[index++];
-}
-
-void AafwkProductAdapter::InitTraceHandlers(EventPrintHandler eventHandler, ErrCodePrintHandler errCodeHandler)
-{
-    sDfxWrapper.eventPrintHandler_ = eventHandler;
-    sDfxWrapper.errCodePrintHandler_ = errCodeHandler;
-}
-
-
-void AafwkProductAdapter::PrintEventTrace(uint8_t info2, uint8_t info3, uint8_t info4)
-{
-    if (sDfxWrapper.eventPrintHandler_ == nullptr || sDfxWrapper.eventTag_ == 0 || sDfxWrapper.eventSubTag_ == 0) {
-        return;
-    }
-
-    uint8_t subTag = (info2 == 0) ? sDfxWrapper.eventSubTag_ : info2;
-    sDfxWrapper.eventPrintHandler_(sDfxWrapper.eventTag_, subTag, info3, info4);
+    EVENT(EC_PUBLIC, 0x3f, info1, info2, info3);
 }
 
 void AafwkProductAdapter::PrintErrCode(uint8_t info2, uint16_t rfu)
 {
-    if (sDfxWrapper.errCodePrintHandler_ == nullptr || sDfxWrapper.errCodeTag_ == 0 ||
-        sDfxWrapper.errCodeSubTag_ == 0) {
-        return;
-    }
-    sDfxWrapper.errCodePrintHandler_(sDfxWrapper.errCodeTag_, sDfxWrapper.errCodeSubTag_, info2, rfu);
+    ERROR_CODE(EC_PUBLIC, 0x3f, 0, info2, rfu);
 }
 } // namespace OHOS
